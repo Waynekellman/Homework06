1d890e4a537523d2b0cea397337f19a2fa516f19
Wayne Kellman <waynekellman@ac.c4q.nyc>
2017-10-24 13:17:36 -0400

    design commit

--- app/src/main/java/nyc/c4q/homework06/MainActivity.java
+++ app/src/main/java/nyc/c4q/homework06/MainActivity.java
@@ -19,30 +19,30 @@ public class MainActivity extends AppCompatActivity {
     }
     public void buttonPress(View v){
         if(v == findViewById(R.id.button01))
-            textView01.setText(textView01.getText().toString() + ((Button) findViewById(R.id.button01)).getText().toString());
-        else if (v== findViewById(R.id.button02))
-            textView01.setText(textView01.getText().toString() + ((Button) findViewById(R.id.button02)).getText().toString());
-        else if (v== findViewById(R.id.button03))
-            textView01.setText(textView01.getText().toString() + ((Button) findViewById(R.id.button03)).getText().toString());
-        else if (v== findViewById(R.id.button04))
-            textView01.setText(textView01.getText().toString() + ((Button) findViewById(R.id.button04)).getText().toString());
-        else if (v== findViewById(R.id.button05))
-            textView01.setText(textView01.getText().toString() + ((Button) findViewById(R.id.button05)).getText().toString());
-        else if (v== findViewById(R.id.button06))
-            textView01.setText(textView01.getText().toString() + ((Button) findViewById(R.id.button06)).getText().toString());
-        else if (v== findViewById(R.id.button07))
-            textView01.setText(textView01.getText().toString() + ((Button) findViewById(R.id.button07)).getText().toString());
-        else if (v== findViewById(R.id.button08))
-            textView01.setText(textView01.getText().toString() + ((Button) findViewById(R.id.button08)).getText().toString());
-        else if (v== findViewById(R.id.button09))
-            textView01.setText(textView01.getText().toString() + ((Button) findViewById(R.id.button09)).getText().toString());
-        else if (v== findViewById(R.id.button00))
-            textView01.setText(textView01.getText().toString() + ((Button) findViewById(R.id.button00)).getText().toString());
+            textView01.setText(textView01.getText().toString() + "1");
+        else if (v == findViewById(R.id.button02))
+            textView01.setText(textView01.getText().toString() + "2");
+        else if (v == findViewById(R.id.button03))
+            textView01.setText(textView01.getText().toString() + "3");
+        else if (v == findViewById(R.id.button04))
+            textView01.setText(textView01.getText().toString() + "4");
+        else if (v == findViewById(R.id.button05))
+            textView01.setText(textView01.getText().toString() + "5");
+        else if (v == findViewById(R.id.button06))
+            textView01.setText(textView01.getText().toString() + "6");
+        else if (v == findViewById(R.id.button07))
+            textView01.setText(textView01.getText().toString() + "7");
+        else if (v == findViewById(R.id.button08))
+            textView01.setText(textView01.getText().toString() + "8");
+        else if (v == findViewById(R.id.button09))
+            textView01.setText(textView01.getText().toString() + "9");
+        else if (v == findViewById(R.id.button00))
+            textView01.setText(textView01.getText().toString() + "0");
         else if (v == findViewById(R.id.button_clear))
             textView01.setText("");
         else if (v== findViewById(R.id.button_par1))
-            textView01.setText(textView01.getText().toString() + ((Button) findViewById(R.id.button_par1)).getText().toString());
+            textView01.setText(textView01.getText().toString() + "(");
         else if (v== findViewById(R.id.button_par2))
-            textView01.setText(textView01.getText().toString() + ((Button) findViewById(R.id.button_par2)).getText().toString());
+            textView01.setText(textView01.getText().toString() + ")");
     }
 }
